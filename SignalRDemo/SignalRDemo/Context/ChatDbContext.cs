86724643c414bdec353c8b37826dd1848fd4a232
Hossam <hossam.mokhtar.fcis@gmail.com>
2026-01-11 08:04:21 +0200

    feat: add Message model, ChatDbContext, and initial migrations
    - Added Message model to store chat messages
    - Created ChatDbContext with Messages DbSet
    - Added initial migration for database setup

---
+++ SignalRDemo/SignalRDemo/Context/ChatDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SignalRDemo.Models;
+
+namespace SignalRDemo.Context
+{
+    public class ChatDbContext(DbContextOptions<ChatDbContext> options) : DbContext(options)
+    {
+        //public ChatDbContext(DbContextOptions<ChatDbContext> options) : base(options)
+        //{
+        //}
+        public DbSet<Message> Messages { get; set; }
+    }
+}
---
+++ SignalRDemo/SignalRDemo/Models/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalRDemo.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Text { get; set; }
+    }
+}
