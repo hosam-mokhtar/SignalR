460914192fc134c7a9653eaa6b13738647929f10
Hossam <hossam.mokhtar.fcis@gmail.com>
2026-01-11 08:05:30 +0200

    feat: implement ChatHub methods for sending messages
    - Added Send method to broadcast messages to other clients

---
+++ SignalRDemo/SignalRDemo/Hubs/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRDemo.Context;
+using SignalRDemo.Models;
+
+namespace SignalRDemo.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly ChatDbContext _context;
+        private readonly ILogger<ChatHub> _logger;
+
+        public ChatHub(ChatDbContext context, ILogger<ChatHub> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task Send(string userName, string message)
+        {
+            await Clients.Others.SendAsync("ReciveMessage", userName, message);
+
+            var msg = new Message
+            {
+                UserName = userName,
+                Text = message
+            };
+
+            _context.Messages.Add(msg);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
